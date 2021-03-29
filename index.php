05792345f80c0599f0dea159790401c99a0530a4
Reza Vaez <reza@balnet.net>
2021-03-29 11:56:06 +0300

    file copy

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <title>Reza Vaez Main Website Page</title>
+  <meta name="description" content="Reza Vaez a person who has a good experience developing Mobile and Web application, SEO Manager, ">
+  <meta name="robots" content="index, follow">
+  <meta name='viewport' content='width=device-width'>
+  <meta charset="UTF-8">
+  <meta name="keywords" content="PHP, SEO, Javascript, HTML, CSS, Ajax, MySql, SqlServer, Linux, Android, Java, Kotlin, Swift">
+  <meta name="author" content="Reza Vaez">
+  <base href="https://mrvaez.com/" target="_self">
+  <link rel="canonical" href="https://mrvaez.com">
+  <link rel="icon" href="favicon.png" type="image/x-icon">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="theme-color" content="white"/>
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="Hello World">
+  <link rel="apple-touch-icon" href="images/icon-180.png">
+  <meta name="msapplication-TileImage" content="images/icon-144.png">
+  <meta name="msapplication-TileColor" content="#FFFFFF">
+  <link rel="preconnect" href="https://mrvaez.com">
+  <link rel="dns-prefetch" href="https://mrvaez.com">
+  <style>
+    html {
+      overflow-x: hidden;
+    }
+
+    body {
+      text-align: center;
+      background-color: #c6c6c6;
+    }
+
+    a {
+      width: 48px;
+      height: 48px;
+      padding: 4px;
+      background-color: #fff;
+      border-radius: 8px;
+    }
+
+    @media (min-width: 360px) {
+      body {
+        margin: 0 auto;
+      }
+    }
+  </style>
+</head>
+<body>
+  <h1>Reza Vaez Personal Website</h1>
+  <a href="https://google.com">google</a><br><br>
+  <a href="https://play.google.com">play store</a><br>
+  <img src="images/reza-vaez.webp" alt="reza vaez personal pic in sunny day" style="margin-top: 24px; width:640px; height: 100%">
+  <script rel="preload" src="js/main.js"></script>
+</body>
+</html>
+<?php
+
+?>
